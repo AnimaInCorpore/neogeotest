83dac8f48e7af3709f3e73ec39954aac1797d604
Sascha Springer <sascha.springer@atomic-skulls.de>
2013-11-03 16:11:40 +0100

    Trace mode added.

--- emulator.asm
+++ emulator.asm
@@ -18,12 +18,21 @@ emulator:
 	lea		old_sp(pc),a1
 	move.l	sp,(a1)																| Save the Atari stack pointer.
 
-	| Set emulator interrupt handlers (in the NeoGeo memory pages!).
+	| Set emulator exception/interrupt handlers (in the NeoGeo memory pages!).
 
-	lea		emulator_vbl_handler(pc),a1
+	lea		bus_error_handler(pc),a1
+	move.l	a1,0x8(a0)
+
+	lea		trace_handler(pc),a1
+	move.l	a1,0x24(a0)
+
+	lea		hbl_handler(pc),a1
+	move.l	a1,0x68(a0)
+
+	lea		vbl_handler(pc),a1
 	move.l	a1,0x70(a0)
 
-	lea		emulator_ikbd_handler(pc),a1
+	lea		ikbd_handler(pc),a1
 	move.l	a1,0x118(a0)
 
 	| Enable the NeoGeo mode.
@@ -42,10 +51,12 @@ emulator:
 
 	lea		0x10F300,sp															| Set the NeoGeo stack pointer.
 
-	move	#0x2300,sr															| Emulation started.
+	move	#0x2000,sr															| Emulation started.
 
 
 
+
+	move	#0xa000,sr															| Trace start.
 
 1:	cmp.b	#0x39,0xfc02.w														| Wait for SPACE.
 	jne		1b
@@ -77,12 +88,223 @@ old_sp:
 mmu_data:
 	ds.l	2
 
-emulator_vbl_handler:
+|-------------------------------------------------------------------------------
+|
+|	Bus error handler.
+|
+|-------------------------------------------------------------------------------
+
+bus_error_handler:
+	move.l	#0xff000000,0x9800.w
+
+	rte
+
+|-------------------------------------------------------------------------------
+|
+|	Trace handler.
+|
+|-------------------------------------------------------------------------------
+
+trace_handler:
+	movem.l	d0-a6,-(sp)
+
+	lea		0x600000,a0
+	lea		character_bitmaps(pc),a1
+
+	move.l	15*4+2(sp),d0
+
+	move	#8-1,d7
+1:
+	rol.l	#4,d0
+	move	d0,d1
+	and		#0xf,d1
+
+	move.l	a1,a2
+
+	move	#8-1,d6
+2:
+	move.b	(a2,d1.w*8),d2
+
+	move	#8-1,d5
+3:
+	add.b	d2,d2
+	scs		d3
+	ext.w	d3
+	move	d3,(a0)+
+
+	dbf		d5,3b
+
+	lea		512*2-8*2(a0),a0
+	addq	#1,a2
+
+	dbf		d6,2b
+
+	lea		-512*2*8+8*2(a0),a0
+
+	dbf		d7,1b
+
+	movem.l	(sp)+,d0-a6
+
+	rte
+
+character_bitmaps:
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00011000
+	dc.b	0b00111000
+	dc.b	0b00011000
+	dc.b	0b00011000
+	dc.b	0b00011000
+	dc.b	0b01111110
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b00001100
+	dc.b	0b00011000
+	dc.b	0b00110000
+	dc.b	0b01111110
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b00001100
+	dc.b	0b00000110
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00001100
+	dc.b	0b00011100
+	dc.b	0b00111100
+	dc.b	0b01101100
+	dc.b	0b01111110
+	dc.b	0b00001100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b01111110
+	dc.b	0b01100000
+	dc.b	0b01111100
+	dc.b	0b00000110
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100000
+	dc.b	0b01111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b01111110
+	dc.b	0b00000110
+	dc.b	0b00001100
+	dc.b	0b00011000
+	dc.b	0b00110000
+	dc.b	0b00110000
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b00111110
+	dc.b	0b00000110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01111110
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b00111100
+	dc.b	0b01100110
+	dc.b	0b01100000
+	dc.b	0b01100000
+	dc.b	0b01100110
+	dc.b	0b00111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b01111100
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01100110
+	dc.b	0b01111100
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b01111110
+	dc.b	0b01100000
+	dc.b	0b01111100
+	dc.b	0b01100000
+	dc.b	0b01100000
+	dc.b	0b01111110
+	dc.b	0b00000000
+
+	dc.b	0b00000000
+	dc.b	0b01111110
+	dc.b	0b01100000
+	dc.b	0b01111100
+	dc.b	0b01100000
+	dc.b	0b01100000
+	dc.b	0b01100000
+	dc.b	0b00000000
+
+|-------------------------------------------------------------------------------
+|
+|	HBL interrupt handler.
+|
+|-------------------------------------------------------------------------------
+
+hbl_handler:
+
+	rte
+
+|-------------------------------------------------------------------------------
+|
+|	VBL interrupt handler.
+|
+|-------------------------------------------------------------------------------
+
+vbl_handler:
 	movem.l	a0-a1,-(sp)
 
 	lea		test_address(pc),a0
 	move.l	(a0),a1
-	move	#0xffff,(a1)+
+	not		(a1)+
 	move.l	a1,(a0)
 
 	movem.l	(sp)+,a0-a1
@@ -92,7 +314,13 @@ emulator_vbl_handler:
 test_address:
 	dc.l	0x600000
 
-emulator_ikbd_handler:
+|-------------------------------------------------------------------------------
+|
+|	IKBD interrupt handler.
+|
+|-------------------------------------------------------------------------------
+
+ikbd_handler:
 
 	rte
 
